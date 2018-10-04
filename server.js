182f7a3ce3cb1b03f19bc29a8dcf327e9c00d1a0
Hibari Khoo <37998538+Hibar3@users.noreply.github.com>
2018-10-05 00:53:09 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 //******MAIN Server******//
 var port= 4000;
+var bodyParser = require('body-parser');
+var SSE = require('sse');
+var path = require('path');
+var fs = require('fs');
 const express = require('express'),
 http = require('http'),
 app = express(),
 server = http.createServer(app),
 io = require('socket.io').listen(server);
-app.get('/', (req, res) => {
 
-res.send('Server is running on port'  + port)
+var clients = [];
+var msg;
+
+//configure body-parser for express
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/',  (req, res) =>  {
+   //res.send('Server is running on port'  + port);
+	//res.send(msg);
+   //res.render('index', {"title":'Hellow'});
+   res.sendFile(path.join(__dirname + '/index.html'));// Opens your main html file
 });
+
 io.on('connection', (socket) => {
 
 console.log('user connected', socket.id)
@@ -22,33 +37,54 @@ socket.on('join', function(userNickname) {
     });
 
 
-socket.on('messagedetection', (senderNickname,messageContent) => {
+socket.on('messagedetection', (Steps,messageContent) => {
        
        //log the message in console 
 
-       console.log(senderNickname+" :" +messageContent)
+       console.log(Steps+" :" +messageContent);
         //create a message object 
-       let  message = {"message":messageContent, "senderNickname":senderNickname}
+       let  message = {"message":messageContent, "Steps":Steps}
+	   
+	   var json = JSON.stringify(Steps+" :" +messageContent);
           // send the message to the client side  
-       io.emit('message', message );
-     
-      });
+       io.emit('message', json );
+	   
+	   //msg=message; //Convert for html usage
+     });
       
   
  socket.on('disconnect', function() {
-    console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") 
+    console.log( ' user has left ');
+    socket.broadcast.emit("userdisconnect"," user has left ") ;
 
 });
 
 });
-
 
 
 //use default IP address
 
-server.listen(4000,()=>{
+server.listen(4000,function() {
+	/**
+   var sse = new SSE(server);
 
-console.log('Node app is running on port ' + port);
+  sse.on('connection', function(stream) {
+    clients.push(stream);
+    console.log('Opened connection ðŸŽ‰');
+
+    var json = JSON.stringify({ message: 'Gotcha' });
+    stream.send(json);
+    console.log('Sent: ' + json);
+
+    stream.on('close', function() {
+      clients.splice(clients.indexOf(stream), 1);
+      console.log('Closed connection ðŸ˜±');
+    });
+  });
+  **/
+  
+  console.log('Node app is running on port ' + port);
 
 });
+
+
